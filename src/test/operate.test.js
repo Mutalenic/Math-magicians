aff284e4c5c4e1bfce6e0fdf9a0d31310ba66f5e
Nicholas Mutale <nicomutale@gmail.com>
2022-04-08 10:42:23 +0200

    fix linter error

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -8,9 +8,9 @@ describe('test operations', () => {
     expect(operate(10, 1, '-')).toBe('9');
   });
   test('test all clear', () => {
-      expect(operate(6, 2, '÷')).toBe('3');
-  })
+    expect(operate(6, 2, '÷')).toBe('3');
+  });
   test('test percentage', () => {
-      expect(operate(9, 2, '%')).toBe('1');
-  })
+    expect(operate(9, 2, '%')).toBe('1');
+  });
 });
